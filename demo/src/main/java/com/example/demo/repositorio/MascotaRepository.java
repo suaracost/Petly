a6b157d24407c7573583b959c01ee1ff9acee313
rinconjr <nicorinconb@yahoo.com>
2024-02-27 16:58:48 -0500

    Arreglos profe

--- demo/src/main/java/com/example/demo/repositorio/MascotaRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/MascotaRepository.java
@@ -19,7 +19,6 @@ public class MascotaRepository {
         data.put(3, new Mascota(3, "Pluto", "Labrador", 5, 30, "Gripe", "https://img.freepik.com/fotos-premium/perrito-lindo-pequeno-posando-alegre-aislado-gris_155003-42660.jpg", "Adoptado"));
         data.put(4, new Mascota(4, "Max", "Beagle", 6, 18, "Healthy", "https://media.istockphoto.com/id/636475496/es/foto/retrato-de-cachorro-marr%C3%B3n-con-fondo-bokeh.jpg?s=612x612&w=0&k=20&c=xVLp2lHN7AhVyTMBXWTf1yfRyJovXT1R0hUwk98Riw8=", "Disponible"));
         data.put(5, new Mascota(5, "Charlie", "Poodle", 8, 12, "Healthy", "https://cdn.pixabay.com/photo/2019/02/06/15/18/puppy-3979350_640.jpg", "Disponible"));
-        data.put(6, new Mascota(6, "Rocky", "Boxer", 9, 28, "Healthy", "https://www.ngenespanol.com/wp-content/uploads/2023/12/descubren-que-los-humanos-influimos-en-el-color-de-ojos-de-los-perros-770x431.jpg", "Adoptado"));
     }
 
     //Metodos para mostrar las mascotas (queries)
