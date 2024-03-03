524e652fbbd5cc066aa5fb2cd7c95b826f9e11d7
Shermann103 <hhermann103@gmail.com>
2024-03-03 14:45:37 -0500

    1ras Correcciones Co-Evaluación
    - SideBar desplegable ya no por clic sino por acercar el mouse (hover).
    - Carruseles infinitos para las secciones Servicios y Veterinarios.
    - Para la sección Servicios (en la Landing) cambio del color del titulo y flechitas a blanco.
    - Para la sección Veterinarios (en la Landing) se quito el degradado de las card y se dejo solor solido

--- demo/src/main/resources/static/js/script.js
+++ demo/src/main/resources/static/js/script.js
@@ -1,19 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selector para el carrusel de servicios
-    const carruselServicios = document.querySelector('.carrusel:not(.carrusel-veterinarios)');
-    document.querySelector('.carrusel-control.izquierda:not(.carrusel-veterinarios-control)').addEventListener('click', function() {
-      carruselServicios.scrollBy({ left: -432, behavior: 'smooth' });
-    });
-    document.querySelector('.carrusel-control.derecha:not(.carrusel-veterinarios-control)').addEventListener('click', function() {
-      carruselServicios.scrollBy({ left: 432, behavior: 'smooth' });
-    });
+  
+  const carrusel = document.querySelector(".carruselServ");
+  let carruselItems = document.querySelectorAll(".card");
+  let carruselItemLast = carruselItems[carruselItems.length - 1];
+  
+  const btnLeft = document.querySelector("#btn-left-serv");
+  const btnRight = document.querySelector("#btn-right-serv");
+  
+  carrusel.insertAdjacentElement("afterbegin", carruselItemLast);
+  
+  function Next() {
+    let carruselItemFirst = document.querySelectorAll(".card")[0];
+    carrusel.style.marginLeft = "-864px";
+    carrusel.style.transition = "all 0.5s";
+    setTimeout(function () {
+      carrusel.style.transition = "none";
+      carrusel.insertAdjacentElement("beforeend", carruselItemFirst);
+      carrusel.style.marginLeft = "-432px";
+    }, 500);
+  }
 
-    // Selector para el carrusel de veterinarios
-    const carruselVeterinarios = document.querySelector('.carrusel-veterinarios');
-    document.querySelector('.carrusel-veterinarios-control.izquierda').addEventListener('click', function() {
-      carruselVeterinarios.scrollBy({ left: -432, behavior: 'smooth' });
-    });
-    document.querySelector('.carrusel-veterinarios-control.derecha').addEventListener('click', function() {
-      carruselVeterinarios.scrollBy({ left: 432, behavior: 'smooth' });
-    });
+  function Prev() {
+    let carruselItem = document.querySelectorAll(".card");
+    let carruselItemLast = carruselItem[carruselItem.length - 1];
+    carrusel.style.marginLeft = "0px";
+    carrusel.style.transition = "all 0.5s";
+    setTimeout(function () {
+      carrusel.style.transition = "none";
+      carrusel.insertAdjacentElement("afterbegin", carruselItemLast);
+      carrusel.style.marginLeft = "-432px";
+    }, 500);
+  }
+  
+  btnRight.addEventListener("click", function () {
+    Next();
+  })
+
+  btnLeft.addEventListener("click", function () {
+    Prev();
+  })
+
+  const carruselVet = document.querySelector(".carruselVet");
+  let carruselItemsVet = document.querySelectorAll(".vetCard");
+  let carruselItemLastVet = carruselItemsVet[carruselItemsVet.length - 1];
+
+  const btnLeftVet = document.querySelector("#btn-left-vet");
+  const btnRightVet = document.querySelector("#btn-right-vet");
+
+  carruselVet.insertAdjacentElement("afterbegin", carruselItemLastVet);
+
+  function NextVet() {
+    let carruselItemFirstVet = document.querySelectorAll(".vetCard")[0];
+    carruselVet.style.marginLeft = "-864px";
+    carruselVet.style.transition = "all 0.5s";
+    setTimeout(function () {
+      carruselVet.style.transition = "none";
+      carruselVet.insertAdjacentElement("beforeend", carruselItemFirstVet);
+      carruselVet.style.marginLeft = "-432px";
+    }, 500);
+  }
+
+  function PrevVet() {
+    let carruselItemVet = document.querySelectorAll(".vetCard");
+    let carruselItemLastVet = carruselItemVet[carruselItemVet.length - 1];
+    carruselVet.style.marginLeft = "0px";
+    carruselVet.style.transition = "all 0.5s";
+    setTimeout(function () {
+      carruselVet.style.transition = "none";
+      carruselVet.insertAdjacentElement("afterbegin", carruselItemLastVet);
+      carruselVet.style.marginLeft = "-432px";
+    }, 500);
+  }
+
+  btnRightVet.addEventListener("click", function () {
+    NextVet();
+  })
+
+  btnLeftVet.addEventListener("click", function () {
+    PrevVet();
+  })
+
 });
