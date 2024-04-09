a3e78e48b5360b478df9711d9f9e7b5d47bcf6bc
rinconjr <nicorinconb@yahoo.com>
2024-04-09 18:05:58 -0500

    Arreglo peticion profe

--- demo/src/main/java/com/example/demo/controlador/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controlador/UsuarioController.java
@@ -1,20 +1,20 @@
 package com.example.demo.controlador;
 
+import com.example.demo.entidad.Cliente;
+import com.example.demo.entidad.Mascota;
+import com.example.demo.servicio.ClienteService;
+import com.example.demo.servicio.MascotaService;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entidad.Cliente;
-import com.example.demo.entidad.Mascota;
-import com.example.demo.servicio.ClienteService;
-import com.example.demo.servicio.MascotaService;
 
 @RestController
 @RequestMapping("usuario")
@@ -38,10 +38,21 @@ public class UsuarioController {
 
     //http://localhost:8090/usuario/1/mascota/1
     //Esta funcion trae los datos de la mascota asociada al usuario.
-    //TODO: Preguntar al profe, como se hace para traer la mascota con tal id.
+    //TODO: Preguntar al profe, como se hace para traer la mascota con tal id. YA
     @GetMapping("/{usuario}/mascota/{id}")
     public Cliente showPet(Model model, @PathVariable("usuario") Long usuario, @PathVariable("id") Long identificacion) {
-        Cliente cliente = clienteService.SearchById(identificacion);
+        
+        Cliente cliente = clienteService.SearchById(usuario);
+
+        Mascota mascota = mascotaService.SearchById(identificacion);
+
+        cliente.setMascotas(new ArrayList<>() );
+
+        if(mascota.getCliente().getCedula() == cliente.getCedula()){
+            cliente.getMascotas().add(mascota);
+
+        };
+        
 
         return cliente;
 
