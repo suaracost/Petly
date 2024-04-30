0ae16031934b5a8daa4d9d21a1aab68980e3c031
Shermann103 <hhermann103@gmail.com>
2024-04-29 23:46:20 -0500

    Mas pruebas Backend
    - Pruebas al servicio Tratamiento con Mock (que ya funcionan)
    
    TOTAL pruebas hasta ahora: 17 / 22
    
    FALTA:
    - Pruebas a un controlador con Mock (el controlador debe tener min5 consiltas)

--- demo/src/test/java/com/example/demo/service/TratamientoServiceTestMock.java
+++ demo/src/test/java/com/example/demo/service/TratamientoServiceTestMock.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions; 
 import org.junit.jupiter.api.Test;
@@ -67,14 +68,12 @@ public class TratamientoServiceTestMock {
         
         Cliente cliente1 = new Cliente("1000000001", "Pedro", "Duran", "pedro@gmail.com", "12345671");
         clienteRepository.save(cliente1);
-
-        // asociar la mascota con el cliente
+        
         mascota1.setCliente(cliente1);
         mascotaRepository.save(mascota1);
         
         Veterinario veterinario2 = new Veterinario("1000000051", "Alexander", "García", "Petly123", "https://universidadeuropea.com/resources/media/images/medicina-veterinaria-1200x630.original.jpg", "General", 0, "Disponible");
         veterinarioRepository.save(veterinario2);
-
 
         Droga droga = new Droga();
         droga.setNombre("ACOLAN");
@@ -83,8 +82,6 @@ public class TratamientoServiceTestMock {
         droga.setUnidadesDisponibles(4);
         droga.setUnidadesVendidas(0);
         drogaRepository.save(droga);
-
-        // crear un tratamiento que asocie una mascota con un veterinario y una droga
         
         String fecha = 29 + "-" + 4 + "-2024";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy");
@@ -100,14 +97,154 @@ public class TratamientoServiceTestMock {
         when(tratamientoRepository.save(tratamiento)).thenReturn(
             tratamiento
         );
+
+        tratamientoService.add(tratamiento);
+        when(tratamientoRepository.findById(tratamiento.getId())).thenReturn( //
+            java.util.Optional.of(tratamiento)
+        );
         
 
         //act
 
-        List<Tratamiento> tratamientos = tratamientoService.SearchAll();
+        Optional<Tratamiento> tratamientos = tratamientoRepository.findById(tratamiento.getId());
 
         //assert
-        Assertions.assertThat(tratamientos.size()).isEqualTo(1);
+        Assertions.assertThat(tratamientos).isNotNull();
+    }
+
+    @Test
+    public void TratamientoService_findAll_TratamientoList() {
+        //arrange        
+        Mascota mascota1 = new Mascota("Firulais", "Criollo", 13, 33, "Esquiso", "https://ih1.redbubble.net/image.1304494203.2178/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "Disponible");
+        mascotaRepository.save(mascota1);
+        
+        Cliente cliente1 = new Cliente("1000000001", "Pedro", "Duran", "pedro@gmail.com", "12345671");
+        clienteRepository.save(cliente1);
+        
+        mascota1.setCliente(cliente1);
+        mascotaRepository.save(mascota1);
+        
+        Veterinario veterinario2 = new Veterinario("1000000051", "Alexander", "García", "Petly123", "https://universidadeuropea.com/resources/media/images/medicina-veterinaria-1200x630.original.jpg", "General", 0, "Disponible");
+        veterinarioRepository.save(veterinario2);
+        
+        // Droga para el tratamiento1
+        Droga droga1 = new Droga();
+        droga1.setNombre("ACOLAN");
+        droga1.setPrecioVenta(151300.0f);
+        droga1.setPrecioCompra(60520.0f);
+        droga1.setUnidadesDisponibles(4);
+        droga1.setUnidadesVendidas(0);
+        drogaRepository.save(droga1);
+        // Droga para el tratamiento2
+        Droga droga2 = new Droga();
+        droga2.setNombre("Z-19");
+        droga2.setPrecioVenta(150600.0f);
+        droga2.setPrecioCompra(60240.0f);
+        droga2.setUnidadesDisponibles(10);
+        droga2.setUnidadesVendidas(0);
+        drogaRepository.save(droga2);
+        
+        
+        String fecha = 29 + "-" + 4 + "-2024";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy");
+        LocalDate fechaLocalDate = LocalDate.parse(fecha, formatter);
+        
+        veterinario2.setNumAtenciones(veterinario2.getNumAtenciones() + 2);
+        veterinarioRepository.save(veterinario2);
+                
+        
+        // Guardar el nuevo tratamiento en la base de datos
+        Tratamiento tratamiento = new Tratamiento(fechaLocalDate, mascota1, veterinario2, droga1);
+        when(tratamientoRepository.save(tratamiento)).thenReturn(
+            tratamiento
+        );
+
+        Tratamiento tratamiento2 = new Tratamiento(fechaLocalDate, mascota1, veterinario2, droga2);
+
+
+        tratamientoService.add(tratamiento);
+        when(tratamientoRepository.findAll()).thenReturn(
+            List.of(tratamiento,tratamiento2)
+        );
+        
+
+        //act
+        List<Tratamiento> tratamientos = tratamientoRepository.findAll();
+
+        //assert
+        Assertions.assertThat(tratamientos).isNotNull();
+        Assertions.assertThat(tratamientos).size().isEqualTo(2);
+    }
+
+    @Test
+    public void TratamientoService_findByveterinarioId_TratamientoList() {
+        //arrange        
+        Mascota mascota1 = new Mascota("Firulais", "Criollo", 13, 33, "Esquiso", "https://ih1.redbubble.net/image.1304494203.2178/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "Disponible");
+        mascotaRepository.save(mascota1);
+        
+        Cliente cliente1 = new Cliente("1000000001", "Pedro", "Duran", "pedro@gmail.com", "12345671");
+        clienteRepository.save(cliente1);
+        
+        mascota1.setCliente(cliente1);
+        mascotaRepository.save(mascota1);
+        
+        Veterinario veterinario2 = new Veterinario("1000000051", "Alexander", "García", "Petly123", "https://universidadeuropea.com/resources/media/images/medicina-veterinaria-1200x630.original.jpg", "General", 0, "Disponible");
+        veterinarioRepository.save(veterinario2);
+        
+        // Droga para el tratamiento1
+        Droga droga1 = new Droga();
+        droga1.setNombre("ACOLAN");
+        droga1.setPrecioVenta(151300.0f);
+        droga1.setPrecioCompra(60520.0f);
+        droga1.setUnidadesDisponibles(4);
+        droga1.setUnidadesVendidas(0);
+        drogaRepository.save(droga1);
+        // Droga para el tratamiento2
+        Droga droga2 = new Droga();
+        droga2.setNombre("Z-19");
+        droga2.setPrecioVenta(150600.0f);
+        droga2.setPrecioCompra(60240.0f);
+        droga2.setUnidadesDisponibles(10);
+        droga2.setUnidadesVendidas(0);
+        drogaRepository.save(droga2);
+        // Droga para el tratamiento3
+        Droga droga3 = new Droga();
+        droga3.setNombre("BACIPREMIX");
+        droga3.setPrecioVenta(188700.0f);
+        droga3.setPrecioCompra(75480.0f);
+        droga3.setUnidadesDisponibles(10);
+        droga3.setUnidadesVendidas(0);
+        drogaRepository.save(droga3);
+        
+        
+        String fecha = 29 + "-" + 4 + "-2024";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy");
+        LocalDate fechaLocalDate = LocalDate.parse(fecha, formatter);
+        
+        veterinario2.setNumAtenciones(veterinario2.getNumAtenciones() + 3);
+        veterinarioRepository.save(veterinario2);
+                
+        
+        // Guardar el nuevo tratamiento en la base de datos
+        Tratamiento tratamiento = new Tratamiento(fechaLocalDate, mascota1, veterinario2, droga1);
+        Tratamiento tratamiento2 = new Tratamiento(fechaLocalDate, mascota1, veterinario2, droga2);
+        Tratamiento tratamiento3 = new Tratamiento(fechaLocalDate, mascota1, veterinario2, droga3);
+        tratamientoRepository.save(tratamiento);
+        tratamientoRepository.save(tratamiento2);
+        tratamientoRepository.save(tratamiento3);
+
+        tratamientoService.findByveterinarioId(veterinario2.getId());
+        when(tratamientoRepository.findByveterinarioId(veterinario2.getId())).thenReturn(
+            List.of(tratamiento,tratamiento2,tratamiento3)
+        );
+
+        //act
+        List<Tratamiento> tratamientos = tratamientoRepository.findByveterinarioId(veterinario2.getId());
+
+        //assert
+        Assertions.assertThat(tratamientos).isNotNull();
+        Assertions.assertThat(tratamientos).size().isEqualTo(3);
+
     }
     
 }
